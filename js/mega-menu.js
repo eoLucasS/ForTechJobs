2e88e666fb6017e5cf8fb94312e01c5d38454b77
eoLucasS <lucassteamgames1419@gmail.com>
2023-04-27 18:47:54 -0300

    edit 1.0

---
+++ js/mega-menu.js
@@ -0,0 +1,7 @@
+(function($) {
+    "use strict";
+    
+    // Mega menu button
+    $('.dropdown-mega-menu-toggle').closest('.menu-item').addClass('megamenu menu-item-has-children');
+
+})(jQuery);
